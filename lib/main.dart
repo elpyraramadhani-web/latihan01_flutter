773dc57587c1c5b5052b6e6007c88d7ec55e31c5
Alvira <elpyraramadhani@gmail.com>
2026-08-04 08:39:30 +0700

    LKPD-1 model data - uji skenario diskon

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 void main() {
   //Deklarasi
-  String nama_barang = "Buku Tulis";
-  double harga_anggota = 3500.0;
-  double harga_umum = 4000.0;
-  int jumlah_stok = 50;
-  bool tersedia = true;
-  int jumlah_beli= 3;
+  String nama_barang = "Mie Ayam";
+  double harga_anggota = 8000.0;
+  double harga_umum = 10000.0;
+  int jumlah_stok = 34;
+  int jumlah_beli= 5;
   double total_anggota = jumlah_beli * harga_anggota;
   double total_umum = jumlah_beli * harga_umum;
   double selisih_harga = total_umum - total_anggota;
 
+
+  bool tersedia = jumlah_stok > 0;
+  bool anggota = false; //false = pembeli umum, true = pembeli anggota
+
+  String kategori = "Atk";
+  String lokasi_rak;
+  switch (kategori) {
+    case "Atk":
+      lokasi_rak = "Rak 1";
+      break;
+    case "Makanan":
+      lokasi_rak = "Rak 2";
+      break;
+    case "Minuman":
+      lokasi_rak = "Rak 3";
+      break;
+    default:
+      lokasi_rak = "Rak Lain";
+  }
+//Mengapa switch case digunakan pada program ini dari pada if else?
+//Karena switch case lebih efisien dan lebih mudah dibaca 
+//ketika ada banyak kondisi yang harus diperiksa berdasarkan nilai dari satu variabel.
+//serta menambah kategori baru di masa
+//depan cukup menambah satu blok "case" baru tanpa mengubah struktur if-else yang ada
+  
+  //Format Rupiah dengan package intl
+  final formatRupiah = NumberFormat.currency(
+    locale: 'id_ID', 
+    symbol: 'Rp ', 
+    decimalDigits: 0);
+
+  //Mengecek pembeli termasuk angggota/umum
+  double harga_satuan;
+  if (anggota) {
+    harga_satuan = harga_anggota;
+  } else {
+    harga_satuan = harga_umum;
+  }
+
+  //menghitung total belanja sebelum potongan
+  double total_belanja = jumlah_beli * harga_satuan;
+
+  //logika diskon
+  double persen_diskon ;
+  if (total_belanja >= 200000) {
+    persen_diskon = 0.10;
+  } else if (total_belanja >= 100000) {
+    persen_diskon = 0.05;
+  } else {
+    persen_diskon = 0.0;
+  }
+
+  //menghitung total belanja setelah potongan
+  double total_setelah_diskon = total_belanja * persen_diskon; 
+  double total_bayar = total_belanja - total_setelah_diskon;
+
 //Output
-print("=====Nama Barang=====");
+print("==========STRUK PEMBELIAN===========");
 print("Nama Barang : $nama_barang");
-print("Harga Anggota : $harga_anggota");
-print("Harga Umum : $harga_umum");
+print('Kategori : $kategori');
+print('Lokasi Rak : $lokasi_rak');
+print("Harga Anggota : ${formatRupiah.format(harga_anggota)}");
+print("Harga Umum : ${formatRupiah.format(harga_umum)}");
 print("Jumlah Stok : $jumlah_stok");
-print("Tersedia : $tersedia");
-print("Total (anggota) $jumlah_beli pcs : $total_anggota");
-print("Selisih vs Umum : $selisih_harga");
-print("======================");
+
+  if (tersedia) {
+  print("Tersedia : Ya (Stok masih ada)");
+}else {
+    print("Tersedia : Tidak (Stok habis)");
+  }
+
+print("Total (anggota) $jumlah_beli pcs : ${formatRupiah.format(total_anggota)}");
+print("Selisih vs Umum : ${formatRupiah.format(selisih_harga)}");
+
+print('-----------TOTAL FINAL------------');
+print('status pembeli : ${anggota ? "Anggota" : "Umum"}');
+print('Jumlah beli : $jumlah_beli pcs');
+print('Harga satuan : ${formatRupiah.format(harga_satuan)}');
+print('Total belanja : ${formatRupiah.format(total_belanja)}');
+print("Potongan Borongan : ${(persen_diskon * 100).toInt()}% (${formatRupiah.format(total_setelah_diskon)})");
+print("Total Bayar : ${formatRupiah.format(total_bayar)}");
+print("======================================");
 
   runApp(const MyApp());
 }
